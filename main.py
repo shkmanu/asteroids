4f9c699eeba20d32f510cdfecb9d022d5ee8bc7f
shkmanu <manu.candia@gmail.com>
2024-10-30 17:41:29 -0400

    D: draw player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -14,12 +15,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
+        player.draw(screen)
         pygame.display.flip()
         fps_clock.tick(60)
 
